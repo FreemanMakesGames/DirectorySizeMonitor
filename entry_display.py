53215a54fe40f75bcddb0a5cee483e73909361e7
Freeman <freeman@freemanmakesgames.pro>
2020-09-25 21:36:06 -0800

    Copyright notice and license notice

--- entry_display.py
+++ entry_display.py
@@ -2,6 +2,24 @@ from entry_interface import *
 
 import os
 import tkinter as tk
+# Copyright 2020 FreemanMakesGames https://www.freemanmakesgames.pro
+
+# This file is part of DirectorySizeMonitor.
+#
+# DirectorySizeMonitor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DirectorySizeMonitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DirectorySizeMonitor. If not, see <https://www.gnu.org/licenses/>.
+
+
 import tkinter.ttk as ttk
 
 
--- entry_infos_display.py
+++ entry_infos_display.py
@@ -1,3 +1,21 @@
+# Copyright 2020 FreemanMakesGames https://www.freemanmakesgames.pro
+
+# This file is part of DirectorySizeMonitor.
+#
+# DirectorySizeMonitor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DirectorySizeMonitor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DirectorySizeMonitor. If not, see <https://www.gnu.org/licenses/>.
+
+
 from entry_display import EntryDisplay
 
 #from entry_info import *
